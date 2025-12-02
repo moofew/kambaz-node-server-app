8a9fd7386736a8957b27a4fe8235d3e32f50f330
Matthew Tong <mtong0722@gmail.com>
2025-12-02 06:18:30 -0500

    finished a6

---
+++ Kambaz/Modules/schema.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import moduleSchema from "../Modules/schema.js";
+const courseSchema = new mongoose.Schema({
+   _id: String,
+   name: String,
+   number: String,
+   credits: Number,
+   description: String,
+   modules: [moduleSchema]
+ },
+ { collection: "courses" }
+);
+const schema = new mongoose.Schema(
+  {
+    _id: String,
+    name: String,
+    description: String,
+    lessons: [{ _id: String, name: String, description: String }],
+  }
+);
+
+export default schema;
